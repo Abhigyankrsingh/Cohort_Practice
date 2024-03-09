072c6b05ca21ba4cd46f9b566f576312f5801535
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-09 13:29:03 +0530

    Code_Updated

--- week1/main.js
+++ week1/main.js
@@ -1,7 +1,23 @@
-let FirsName = "Abhigyan";
+// let FirsName = "Abhigyan";
 
-let age = 20;
+// let age = 20;
 
-let isMarried = false;
+// let isMarried = false;
 
-console.log("This person name is " + FirsName + " and his age is " + age);
+// console.log("This person name is " + FirsName + " and his age is " + age);
+
+// if(isMarried == true){
+//     console.log("This person is married");
+// } else {
+//     console.log("This person is not married");
+// }
+
+
+let answer = 0;
+
+for(let i =0; i<5;i++){
+    
+    answer = answer + i;
+}
+
+console.log(answer);
