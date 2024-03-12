32af85269afbb0dc8ad32cb535715e7e639a6370
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-12 18:39:17 +0530

    Code_Updated

--- week1/main.js
+++ week1/main.js
@@ -108,12 +108,31 @@
 // str.lastIndexOf(target);
 
 
-function getSlice(str, start, end){
+// function getSlice(str, start, end){
 
-    console.log("Original String:", str);
+//     console.log("Original String:", str);
 
-    console.log("Original Slice:", str.slice(start, end));
+//     console.log("Original Slice:", str.slice(start, end));
 
+// }
+
+// getSlice("Hello World", 0, 5);
+
+function cutIT(str, startIndex, endIndex){
+    let newStr = "";
+
+    for(let i =0; i<str.length; i++){
+        if( i>=startIndex && i <endIndex) {
+            newStr = newStr + str[i];
+        }
+    }
+
+    return newStr;
 }
 
-getSlice("Hello World", 0, 5);
+const value = "Abhigyan Singh";
+
+let ans2 = value.slice(2,5);
+console.log(ans2);
+
+console.log(cutIT(value,2,5));
