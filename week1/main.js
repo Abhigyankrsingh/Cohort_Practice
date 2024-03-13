95375984c7ab4fb60500faa1c2a9a6d9a47f16f7
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-14 01:40:23 +0530

    Code_updated

--- week1/main.js
+++ week1/main.js
@@ -305,15 +305,39 @@
 
 // console.log("Hello World");
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.readFile("a.txt","utf-8", function(err, data){
+// fs.readFile("a.txt","utf-8", function(err, data){
 
 
-    console.log(data);
-})
+//     console.log(data);
+// })
 
-console.log("Hi");
+// console.log("Hi");
+
+
+function square(n){
+    return n *n;
+}
+
+function cube(n){
+    return n *n*n;
+}
+
+
+function sumOfSquare(a, b, fn){
+    const val1 = fn(a);
+    const val2 = fn(b);
+
+    return val1 + val2;
+}
+
+
+function sumOfcubes(a,b,fn){
+    const val1 = fn(a);
+    const val2 = fn(b);
+    return val1 + val2;
+}
 
 
 
