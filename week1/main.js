21a08cbbe119556a009bb13eeae47c3a9f84f25a
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-14 01:08:23 +0530

    Code_updated

--- week1/main.js
+++ week1/main.js
@@ -285,3 +285,27 @@
 // }
 
 // console.log(ans);
+
+
+function findSum(n){
+
+    let ans = 0;
+
+    for(let i =0 ; i< n; i++){
+        ans +=i;
+    }
+   return ans;
+}
+
+function findSumtill100(){
+    console.log( findSum(100));
+}
+
+setTimeout(findSumtill100,1000)
+
+console.log("Hello World");
+
+
+
+
+
