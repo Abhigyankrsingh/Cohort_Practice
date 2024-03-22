a66bbec025312b7574b71306784d4f9661a1a164
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-22 16:44:11 +0530

    Code_Updated

--- week2/index.js
+++ week2/index.js
@@ -1,27 +1,66 @@
 
-const express = require("express");
+// const express = require("express");
 
 
-const app = express();
+// const app = express();
 
-const port = 3000;
-
-
-app.get('/', (req,res) => {
-
-    res.send('Hello World');
-})
+// const port = 3000;
 
 
-app.post('backend-api/conversation', function(req,res){
+// app.get('/', (req,res) => {
 
-  res.send("Hii");
+//     res.send('Hello World');
+// })
+
+
+// app.post('backend-api/conversation', function(req,res){
+
+//   res.send("Hii");
     
+// })
+
+
+
+// app.listen(port,()=> {
+
+//     console.log(` We are listeing at ${port}`);
+// })
+
+
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get("/app-router", function(req,res){
+
+  res.json ({
+     name: "Abhi",
+     age: 21
+  })
+})
+
+
+app.get('/', (req,res) =>{
+
+  res.send("Hello World")
+})
+
+
+app.listen(port, () =>{
+
+  console.log(`This app is running at ${port}`)
 })
 
 
 
-app.listen(port,()=> {
 
-    console.log(` We are listeing at ${port}`);
-})
+
+
+
+
+
+
+
+
