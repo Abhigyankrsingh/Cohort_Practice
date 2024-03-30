269ea672a20de329e69e1b8ca1b9b7cb1ad1aa03
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-30 16:27:54 +0530

    Code_Updated

--- week2/index.js
+++ week2/index.js
@@ -57,24 +57,77 @@
 
 //Noraml function
 
-function abhiAsyncfunc(){
+// function abhiAsyncfunc(){
+//  let p = new Promise(function(resolve){
+//   resolve("Hi there! ")
+//   });
+//   return p;
+// }
+// function main() {
+//   abhiAsyncfunc().then(function(value){
+//     console.log(value);
+//   });
+// }
+
+// main();
+
+// function abhiAyfunc() {
+//   let pi = new Promise(function(resolve){
+//      resolve("Hello Mintu")
+
+//   });
+
+//   return pi;
+// }
+
+// function main() {
+//   abhiAyfunc().then(function(value){
+//     console.log(value);
+//   });
+// }
+
+
+// main();
+
+// function abhids(){
+//   let df = new Promise(function(resolve){
+//     resolve("hi fo")
+//   });
+
+//   return df;
+// }
+
+// async function main(){
+//   const value = await abhids();
+//   console.log(value);
+// }
+
+// main();
+
+
+// function mintAsync(){
+
+//   let p = new Promise(function(resolve){
+//     resolve("Hi there")
+//   });
+//   return p;
+// }
+
+
+function kiratAsyncFunction(){
 
   let p = new Promise(function(resolve){
-
-    resolve("Hi there! ")
+    setTimeout(function(){
+      resolve("Hi there")
+    },3000)
   });
   return p;
 }
 
-function main() {
-  abhiAsyncfunc().then(function(value){
-    console.log(value);
-  });
+async function main(){
+
+  let value = await kiratAsyncFunction();
+  console.log(value);
 }
 
-
 main();
-
-
-
-
